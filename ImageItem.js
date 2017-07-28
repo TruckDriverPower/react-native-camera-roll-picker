fc2da5989772c6ff579345cd952b4326f09cca5d
Jose Gomez <jose4gga@gmail.com>
2017-07-28 10:41:45 -0400

    Migrating from React.PropTypes

--- ImageItem.js
+++ ImageItem.js
@@ -1,45 +1,33 @@
-import React, {Component} from 'react';
-import {
-  Image,
-  StyleSheet,
-  Dimensions,
-  TouchableOpacity,
-} from 'react-native';
+import React, { Component } from "react";
+import { Image, StyleSheet, Dimensions, TouchableOpacity } from "react-native";
+import PropTypes from "prop-types";
 
 class ImageItem extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
 
   componentWillMount() {
-    var {width} = Dimensions.get('window');
-    var {imageMargin, imagesPerRow, containerWidth} = this.props;
+    var { width } = Dimensions.get("window");
+    var { imageMargin, imagesPerRow, containerWidth } = this.props;
 
-    if(typeof containerWidth != "undefined") {
+    if (typeof containerWidth != "undefined") {
       width = containerWidth;
     }
     this._imageSize = (width - (imagesPerRow + 1) * imageMargin) / imagesPerRow;
   }
 
   render() {
-    var {item, selected, selectedMarker, imageMargin} = this.props;
+    var { item, selected, selectedMarker, imageMargin } = this.props;
 
-    var marker = selectedMarker ? selectedMarker :
-        <Image
-          style={[styles.marker, {width: 25, height: 25}]}
-          source={require('./circle-check.png')}
-          />;
+    var marker = selectedMarker ? selectedMarker : <Image style={[styles.marker, { width: 25, height: 25 }]} source={require("./circle-check.png")} />;
 
     var image = item.node.image;
 
     return (
-      <TouchableOpacity
-        style={{marginBottom: imageMargin, marginRight: imageMargin}}
-        onPress={() => this._handleClick(image)}>
-        <Image
-          source={{uri: image.uri}}
-          style={{height: this._imageSize, width: this._imageSize}} >
-          { (selected) ? marker : null }
+      <TouchableOpacity style={{ marginBottom: imageMargin, marginRight: imageMargin }} onPress={() => this._handleClick(image)}>
+        <Image source={{ uri: image.uri }} style={{ height: this._imageSize, width: this._imageSize }}>
+          {selected ? marker : null}
         </Image>
       </TouchableOpacity>
     );
@@ -52,25 +40,25 @@ class ImageItem extends Component {
 
 const styles = StyleSheet.create({
   marker: {
-    position: 'absolute',
+    position: "absolute",
     top: 5,
     right: 5,
-    backgroundColor: 'transparent',
-  },
-})
+    backgroundColor: "transparent"
+  }
+});
 
 ImageItem.defaultProps = {
   item: {},
-  selected: false,
-}
+  selected: false
+};
 
 ImageItem.propTypes = {
-  item: React.PropTypes.object,
-  selected: React.PropTypes.bool,
-  selectedMarker: React.PropTypes.element,
-  imageMargin: React.PropTypes.number,
-  imagesPerRow: React.PropTypes.number,
-  onClick: React.PropTypes.func,
-}
+  item: PropTypes.object,
+  selected: PropTypes.bool,
+  selectedMarker: PropTypes.element,
+  imageMargin: PropTypes.number,
+  imagesPerRow: PropTypes.number,
+  onClick: PropTypes.func
+};
 
 export default ImageItem;
--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-import React, {Component} from 'react';
-import {
-  CameraRoll,
-  Platform,
-  StyleSheet,
-  View,
-  Text,
-  ListView,
-  ActivityIndicator,
-} from 'react-native';
-import ImageItem from './ImageItem';
+import React, { Component } from "react";
+import { CameraRoll, Platform, StyleSheet, View, Text, ListView, ActivityIndicator } from "react-native";
+import PropTypes from "prop-types";
+import ImageItem from "./ImageItem";
 
 class CameraRollPicker extends Component {
   constructor(props) {
@@ -20,7 +13,7 @@ class CameraRollPicker extends Component {
       lastCursor: null,
       loadingMore: false,
       noMore: false,
-      dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2}),
+      dataSource: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
     };
   }
 
@@ -30,23 +23,25 @@ class CameraRollPicker extends Component {
 
   componentWillReceiveProps(nextProps) {
     this.setState({
-      selected: nextProps.selected,
+      selected: nextProps.selected
     });
   }
 
   fetch() {
     if (!this.state.loadingMore) {
-      this.setState({loadingMore: true}, () => { this._fetch(); });
+      this.setState({ loadingMore: true }, () => {
+        this._fetch();
+      });
     }
   }
 
   _fetch() {
-    var {groupTypes, assetType} = this.props;
+    var { groupTypes, assetType } = this.props;
 
     var fetchParams = {
       first: 1000,
       groupTypes: groupTypes,
-      assetType: assetType,
+      assetType: assetType
     };
 
     if (Platform.OS === "android") {
@@ -58,14 +53,13 @@ class CameraRollPicker extends Component {
       fetchParams.after = this.state.lastCursor;
     }
 
-    CameraRoll.getPhotos(fetchParams)
-      .then((data) => this._appendImages(data), (e) => console.log(e));
+    CameraRoll.getPhotos(fetchParams).then(data => this._appendImages(data), e => console.log(e));
   }
 
   _appendImages(data) {
     var assets = data.edges;
     var newState = {
-      loadingMore: false,
+      loadingMore: false
     };
 
     if (!data.page_info.has_next_page) {
@@ -75,78 +69,52 @@ class CameraRollPicker extends Component {
     if (assets.length > 0) {
       newState.lastCursor = data.page_info.end_cursor;
       newState.images = this.state.images.concat(assets);
-      newState.dataSource = this.state.dataSource.cloneWithRows(
-        this._nEveryRow(newState.images, this.props.imagesPerRow)
-      );
+      newState.dataSource = this.state.dataSource.cloneWithRows(this._nEveryRow(newState.images, this.props.imagesPerRow));
     }
 
     this.setState(newState);
   }
 
   render() {
-    var {dataSource} = this.state;
-    var {
-      scrollRenderAheadDistance,
-      initialListSize,
-      pageSize,
-      removeClippedSubviews,
-      imageMargin,
-      backgroundColor,
-      emptyText,
-      emptyTextStyle,
-    } = this.props;
+    var { dataSource } = this.state;
+    var { scrollRenderAheadDistance, initialListSize, pageSize, removeClippedSubviews, imageMargin, backgroundColor, emptyText, emptyTextStyle } = this.props;
 
-    var listViewOrEmptyText = dataSource.getRowCount() > 0 ? (
-      <ListView
-        style={{flex: 1,}}
-        scrollRenderAheadDistance={scrollRenderAheadDistance}
-        initialListSize={initialListSize}
-        pageSize={pageSize}
-        removeClippedSubviews={removeClippedSubviews}
-        renderFooter={this._renderFooterSpinner.bind(this)}
-        onEndReached={this._onEndReached.bind(this)}
-        dataSource={dataSource}
-        renderRow={rowData => this._renderRow(rowData)} />
-    ) : (
-      <Text style={[{textAlign: 'center'}, emptyTextStyle]}>{emptyText}</Text>
-    );
+    var listViewOrEmptyText =
+      dataSource.getRowCount() > 0
+        ? <ListView
+            style={{ flex: 1 }}
+            scrollRenderAheadDistance={scrollRenderAheadDistance}
+            initialListSize={initialListSize}
+            pageSize={pageSize}
+            removeClippedSubviews={removeClippedSubviews}
+            renderFooter={this._renderFooterSpinner.bind(this)}
+            onEndReached={this._onEndReached.bind(this)}
+            dataSource={dataSource}
+            renderRow={rowData => this._renderRow(rowData)}
+          />
+        : <Text style={[{ textAlign: "center" }, emptyTextStyle]}>
+            {emptyText}
+          </Text>;
 
     return (
-      <View
-        style={[styles.wrapper, {padding: imageMargin, paddingRight: 0, backgroundColor: backgroundColor},]}>
+      <View style={[styles.wrapper, { padding: imageMargin, paddingRight: 0, backgroundColor: backgroundColor }]}>
         {listViewOrEmptyText}
       </View>
     );
   }
 
   _renderImage(item) {
-    var {selected} = this.state;
-    var {
-      imageMargin,
-      selectedMarker,
-      imagesPerRow,
-      containerWidth
-    } = this.props;
+    var { selected } = this.state;
+    var { imageMargin, selectedMarker, imagesPerRow, containerWidth } = this.props;
 
     var uri = item.node.image.uri;
-    var isSelected = (this._arrayObjectIndexOf(selected, 'uri', uri) >= 0) ? true : false;
+    var isSelected = this._arrayObjectIndexOf(selected, "uri", uri) >= 0 ? true : false;
 
-    return (
-      <ImageItem
-        key={uri}
-        item={item}
-        selected={isSelected}
-        imageMargin={imageMargin}
-        selectedMarker={selectedMarker}
-        imagesPerRow={imagesPerRow}
-        containerWidth={containerWidth}
-        onClick={this._selectImage.bind(this)}
-      />
-    );
+    return <ImageItem key={uri} item={item} selected={isSelected} imageMargin={imageMargin} selectedMarker={selectedMarker} imagesPerRow={imagesPerRow} containerWidth={containerWidth} onClick={this._selectImage.bind(this)} />;
   }
 
   _renderRow(rowData) {
-    var items = rowData.map((item) => {
+    var items = rowData.map(item => {
       if (item === null) {
         return null;
       }
@@ -174,16 +142,16 @@ class CameraRollPicker extends Component {
   }
 
   _selectImage(image) {
-    var {maximum, imagesPerRow, callback, selectSingleItem} = this.props;
+    var { maximum, imagesPerRow, callback, selectSingleItem } = this.props;
 
     var selected = this.state.selected,
-        index = this._arrayObjectIndexOf(selected, 'uri', image.uri);
+      index = this._arrayObjectIndexOf(selected, "uri", image.uri);
 
     if (index >= 0) {
       selected.splice(index, 1);
     } else {
       if (selectSingleItem) {
-        selected.splice(0,selected.length);
+        selected.splice(0, selected.length);
       }
       if (selected.length < maximum) {
         selected.push(image);
@@ -192,9 +160,7 @@ class CameraRollPicker extends Component {
 
     this.setState({
       selected: selected,
-      dataSource: this.state.dataSource.cloneWithRows(
-        this._nEveryRow(this.state.images, imagesPerRow)
-      ),
+      dataSource: this.state.dataSource.cloneWithRows(this._nEveryRow(this.state.images, imagesPerRow))
     });
 
     callback(selected, image);
@@ -202,7 +168,7 @@ class CameraRollPicker extends Component {
 
   _nEveryRow(data, n) {
     var result = [],
-        temp = [];
+      temp = [];
 
     for (var i = 0; i < data.length; ++i) {
       if (i > 0 && i % n === 0) {
@@ -223,76 +189,67 @@ class CameraRollPicker extends Component {
   }
 
   _arrayObjectIndexOf(array, property, value) {
-    return array.map((o) => { return o[property]; }).indexOf(value);
+    return array
+      .map(o => {
+        return o[property];
+      })
+      .indexOf(value);
   }
-
 }
 
 const styles = StyleSheet.create({
-  wrapper:{
-    flex: 1,
+  wrapper: {
+    flex: 1
   },
-  row:{
-    flexDirection: 'row',
-    flex: 1,
+  row: {
+    flexDirection: "row",
+    flex: 1
   },
   marker: {
-    position: 'absolute',
+    position: "absolute",
     top: 5,
-    backgroundColor: 'transparent',
-  },
-})
+    backgroundColor: "transparent"
+  }
+});
 
 CameraRollPicker.propTypes = {
-  scrollRenderAheadDistance: React.PropTypes.number,
-  initialListSize: React.PropTypes.number,
-  pageSize: React.PropTypes.number,
-  removeClippedSubviews: React.PropTypes.bool,
-  groupTypes: React.PropTypes.oneOf([
-    'Album',
-    'All',
-    'Event',
-    'Faces',
-    'Library',
-    'PhotoStream',
-    'SavedPhotos',
-  ]),
-  maximum: React.PropTypes.number,
-  assetType: React.PropTypes.oneOf([
-    'Photos',
-    'Videos',
-    'All',
-  ]),
-  selectSingleItem: React.PropTypes.bool,
-  imagesPerRow: React.PropTypes.number,
-  imageMargin: React.PropTypes.number,
-  containerWidth: React.PropTypes.number,
-  callback: React.PropTypes.func,
-  selected: React.PropTypes.array,
-  selectedMarker: React.PropTypes.element,
-  backgroundColor: React.PropTypes.string,
-  emptyText: React.PropTypes.string,
-  emptyTextStyle: Text.propTypes.style,
-}
+  scrollRenderAheadDistance: PropTypes.number,
+  initialListSize: PropTypes.number,
+  pageSize: PropTypes.number,
+  removeClippedSubviews: PropTypes.bool,
+  groupTypes: PropTypes.oneOf(["Album", "All", "Event", "Faces", "Library", "PhotoStream", "SavedPhotos"]),
+  maximum: PropTypes.number,
+  assetType: PropTypes.oneOf(["Photos", "Videos", "All"]),
+  selectSingleItem: PropTypes.bool,
+  imagesPerRow: PropTypes.number,
+  imageMargin: PropTypes.number,
+  containerWidth: PropTypes.number,
+  callback: PropTypes.func,
+  selected: PropTypes.array,
+  selectedMarker: PropTypes.element,
+  backgroundColor: PropTypes.string,
+  emptyText: PropTypes.string,
+  emptyTextStyle: Text.propTypes.style
+};
 
 CameraRollPicker.defaultProps = {
   scrollRenderAheadDistance: 500,
   initialListSize: 1,
   pageSize: 3,
   removeClippedSubviews: true,
-  groupTypes: 'SavedPhotos',
+  groupTypes: "SavedPhotos",
   maximum: 15,
   imagesPerRow: 3,
   imageMargin: 5,
   selectSingleItem: false,
-  assetType: 'Photos',
-  backgroundColor: 'white',
+  assetType: "Photos",
+  backgroundColor: "white",
   selected: [],
   callback: function(selectedImages, currentImage) {
     console.log(currentImage);
     console.log(selectedImages);
   },
-  emptyText: 'No photos.',
-}
+  emptyText: "No photos."
+};
 
 export default CameraRollPicker;
